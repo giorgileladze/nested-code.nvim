a504c5db8b804b2189339c20abaee1ddf1463139
giorgileladze <giorgileladze78@gmail.com>
2024-11-03 20:24:23 +0400

    initial commit

--- lua/nested-code/init.lua
+++ lua/nested-code/init.lua
@@ -2,6 +2,20 @@ local M = {}
 
 function M.toggle_menu()
 	print("hello world")
+
+	local bufnr = vim.api.nvim_create_buf(false, true) -- Create a new scratch buffer
+
+	local context = M.get_context()
+
+	vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, { context })
+	vim.api.nvim_open_win(bufnr, true, {
+		relative = "editor",
+		width = vim.o.columns,
+		height = 1,
+		col = 0,
+		row = 0,
+		style = "minimal",
+	})
 end
 
 function M.setup()
